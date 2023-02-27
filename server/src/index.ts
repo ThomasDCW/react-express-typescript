9da83fc91ecfffea2fc42551663c769981aa28a7
Thomas_DCL_USR <thomas.dicola@user-agency.com>
2023-02-27 15:32:36 +0100

    init prisma-postgresql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,42 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { config } from './config';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = express();
+
 app.use(cors());
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello there !');
+// users routes
+
+app.get('/users', async (_req: Request, res: Response) => {
+  const allUsers = await prisma.user.findMany();
+  res.json(allUsers);
+});
+
+app.get('/user/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const userById = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+  res.json(userById);
+});
+
+/// posts routes
+
+app.get('/posts', async (_req: Request, res: Response) => {
+  const allPosts = await prisma.post.findMany();
+  res.json(allPosts);
+});
+
+app.get('/post/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const postById = await prisma.post.findUnique({ where: { id: Number(id) } });
+  res.json(postById);
 });
 
 app.listen(config.PORT, () =>
